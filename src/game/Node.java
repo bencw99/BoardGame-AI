acc19db379d310d932492b1efb384ff18a650a88
bencw99 <ben.cohenwang@gmail.com>
2014-09-02 09:08:52 -0700

    CREATED Node constructors and variables

--- src/game/Node.java
+++ src/game/Node.java
@@ -1,5 +1,7 @@
 package game;
 
+import java.awt.Color;
+
 /**
  * A class representing a checkers board node
  * 
@@ -7,5 +9,29 @@ package game;
  */
 public class Node 
 {
+	/** The board containing this node **/
+	private Board board;
 	
+	/** The piece currently contained in this node **/
+	private Piece piece;
+	
+	/** The color of this node **/
+	private Color color;
+	
+	/**
+	 * Default constructor
+	 */
+	public Node()
+	{
+		piece = null;
+	}
+	
+	/**
+	 * Parameterized constructor, initializes color to given color
+	 */
+	public Node(Color color)
+	{
+		piece = null;
+		this.color = color;
+	}
 }
