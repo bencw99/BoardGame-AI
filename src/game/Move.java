b9877981a2b3f1b29c09dcb5ce270b2045ec503c
bencw99 <ben.cohenwang@gmail.com>
2014-09-14 22:34:59 -0700

    FIXED move class

--- src/game/Move.java
+++ src/game/Move.java
@@ -10,15 +10,9 @@ import java.util.ArrayList;
  * @author Benjamin Cohen-Wang
  */
 public class Move
-{
-	/** The node this move starts at **/
-	private Node start;
-	
-	/** The node this move ends at **/
-	private Node end;
-	
-	/** The array list of nodes between the starting and ending nodes **/
-	private ArrayList<Node> interNodes;
+{	
+	/** The array list of nodes gone through in this move **/
+	private ArrayList<Node> nodes;
 	
 	/** The array list of pieces jumped in this move **/
 	private ArrayList<Piece> jumped;
@@ -29,16 +23,12 @@ public class Move
 	/**
 	 * Parameterized Constructor, initializes start and end nodes, board, and interNodes
 	 * 
-	 * @param start	the node this move starts at
-	 * @param end	the node this move ends at
 	 * @param interNodes	the array list of nodes jumped through
 	 * @param board	the board this move occurs in
 	 */
-	public Move(Node start, Node end, ArrayList<Node> interNodes, Board board)
+	public Move(ArrayList<Node> nodes, Board board)
 	{
-		this.start = start;
-		this.end = end;
-		this.interNodes = interNodes;
+		this.nodes = nodes;
 		this.board = board;
 		
 		loadJumped();
@@ -49,19 +39,12 @@ public class Move
 	 */
 	public void loadJumped()
 	{
-		Node current = start;
+		Node current = nodes.get(0);
 		Node next;
 		
-		for(int i = 0; i <= interNodes.size(); i ++)
+		for(int i = 0; i <= nodes.size(); i ++)
 		{
-			if(i == interNodes.size())
-			{
-				next = end;
-			}
-			else
-			{
-				next = interNodes.get(i);
-			}
+			next = nodes.get(i);
 			
 			if(Math.abs(next.getLoc().getRow() - current.getLoc().getRow()) > 1)
 			{
@@ -79,7 +62,7 @@ public class Move
 	 */
 	public boolean isSimple()
 	{
-		if(interNodes.isEmpty())
+		if(nodes.size() == 2)
 		{
 			return true;
 		}
@@ -104,28 +87,8 @@ public class Move
 	 * 
 	 * @return the array list of nodes moved through
 	 */ 
-	public ArrayList<Node> getInterNodes()
+	public ArrayList<Node> getNodes()
 	{
-		return interNodes;
-	}
-	
-	/**
-	 * Returns the node this move starts at
-	 * 
-	 * @return the node this move starts at
-	 */
-	public Node getStart()
-	{
-		return start;
-	}
-	
-	/**
-	 * Returns the node this move ends at
-	 * 
-	 * @return the node this move ends at
-	 */
-	public Node getEnd()
-	{
-		return end;
+		return nodes;
 	}
 }
