061ed66a9c9aa6f8dbe7fd7208c775a19381410e
bencw99 <ben.cohenwang@gmail.com>
2014-09-14 22:55:24 -0700

    CREATED player class

---
+++ src/game/player/Player.java
@@ -0,0 +1,84 @@
+package game.player;
+import java.util.ArrayList;
+
+import game.piece.*;
+import game.piece.Piece.Loyalty;
+
+/**
+ * A class representing a Player associated with a checkers game
+ * 
+ * @author Benjamin Cohen-Wang
+ */
+public class Player
+{
+	/** The name of this player **/
+	private String name;
+	
+	/** The loyalty of this player **/
+	private Loyalty loyalty;
+	
+	/** The pieces of this player **/
+	ArrayList<Piece> pieces;
+	
+	/**
+	 * Parameterized constructor, initializes name, pieces, and loyalty
+	 * 
+	 * @param name	the name of this player
+	 * @param loyalty	the loyalty of this player
+	 * @param pieces	the pieces of this player
+	 */
+	public Player(String name, Loyalty loyalty, ArrayList<Piece> pieces)
+	{
+		this.name = name;
+		this.loyalty = loyalty;
+		this.pieces = pieces;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName()
+	{
+		return name;
+	}
+
+	/**
+	 * @return the loyalty
+	 */
+	public Loyalty getLoyalty()
+	{
+		return loyalty;
+	}
+
+	/**
+	 * @return the pieces
+	 */
+	public ArrayList<Piece> getPieces()
+	{
+		return pieces;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	/**
+	 * @param loyalty the loyalty to set
+	 */
+	public void setLoyalty(Loyalty loyalty)
+	{
+		this.loyalty = loyalty;
+	}
+
+	/**
+	 * @param pieces the pieces to set
+	 */
+	public void setPieces(ArrayList<Piece> pieces)
+	{
+		this.pieces = pieces;
+	}
+}
