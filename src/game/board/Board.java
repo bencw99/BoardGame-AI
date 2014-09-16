47cb89faf97a608c2ecfa6e3e6b1f699731cdc3d
bencw99 <ben.cohenwang@gmail.com>
2014-09-16 09:46:55 -0700

    ADDED fraw method, other fixes

--- src/game/board/Board.java
+++ src/game/board/Board.java
@@ -1,11 +1,13 @@
 package game.board;
 
+import game.Game;
 import game.Move;
 import game.piece.Piece;
 import game.piece.Piece.Loyalty;
 import game.piece.Soldier;
 
 import java.awt.Color;
+import java.awt.Graphics;
 import java.util.ArrayList;
 
 /**
@@ -18,40 +20,49 @@ public class Board
 	/** The array of nodes composing the board grid **/
 	private Node[][] grid;
 	
+	/** The game this board belong to **/
+	private Game game;
+	
 	/** The default length of the board grid **/
-	public final static int DEFAULT_GRID_LENGTH = 8;
+	public static final int DEFAULT_GRID_LENGTH = 8;
 	
-	/** The default number of pieces on the checkers grid **/
-	public final static int DEFAULT_PIECE_NUM = 12;
+	/** The default width of nodes of this board **/
+	public static final int NODE_WIDTH =  80;
 	
+	/** The default height of nodes of this board **/
+	public static final int NODE_HEIGHT =  80;
+			
 	/**
-	 * Default constructor, initializes grid to size 8 by 8
+	 * Parameterized constructor, initializes grid to size 8 by 8, and game to given game
+	 * 
+	 * @param game	the game of this board
 	 */
-	public Board()
+	public Board(Game game)
 	{
-		this(DEFAULT_GRID_LENGTH, DEFAULT_PIECE_NUM);
+		this(DEFAULT_GRID_LENGTH, game);
 	}
 	
 	/**
 	 * Parameterized constructor, initializes grid to be a square with given side length
 	 * 
 	 * @param length	the side length of the grid
-	 * @param pieceNum	the number of pieces of each player
+	 * @param game	the game of this board
 	 */
-	public Board(int length, int pieceNum)
+	public Board(int length, Game game)
 	{
-		this(length, length, pieceNum);
+		this(length, length, game);
 	}
 	
 	/**
 	 * Parameterized constructor, initializes grid to be a rectangle with given length and width
 	 * 
 	 * @param length	the side length of the grid
-	 * @param pieceNum	the number of pieces of each player
+	 * @param game	the game of this board
 	 */
-	public Board(int length, int width, int pieceNum)
+	public Board(int length, int width, Game game)
 	{
-		grid = new Node[length][width];
+		this.grid = new Node[length][width];
+		this.game = game;
 		
 		for(int i = 0; i < length; i ++)
 		{
@@ -72,23 +83,49 @@ public class Board
 			}
 		}
 		
-		int piecesLeft = pieceNum;
-
-		for(int i = 0; i < length; i ++)
+		loadBoard();
+	}
+	
+	/**
+	 * Loads the board grid
+	 */
+	public void loadBoard()
+	{
+		int[] piecesLeft = new int[game.getPlayers().length];
+		
+		for(int i = 0; i < piecesLeft.length; i ++)
 		{
-			for(int j = 0; j < width; j ++)
-			{
+			piecesLeft[i] = game.getPlayers()[i].getPieces().size();
+		}
+
+		for(int i = 0; i < grid.length; i ++)
+		{
+			for(int j = 0; j < grid[0].length; j ++)
+			{	
 				if(grid[i][j].getColor() == Color.BLACK)
 				{
-					grid[i][j].add(new Soldier(Loyalty.RED, grid[i][j]));
-					grid[length - 1 - i][width - 1 - j].add(new Soldier(Loyalty.BLACK, grid[length - 1 - i][width - 1 - j]));
-					
-					piecesLeft --;
-					
-					if(piecesLeft <= 0)
+					if(piecesLeft[0] > 0)
 					{
-						break;
+						add(game.getPlayers()[0].getPieces().get(piecesLeft[0] - 1), grid[i][j].getLoc());
 					}
+						
+					piecesLeft[0] --;
+				}
+			}
+		}
+		
+		for(int i = grid.length - 1; i >= 0; i --)
+		{
+			for(int j = grid[0].length; j >= 0; j --)
+			{	
+				if(grid[i][j].getColor() == Color.BLACK)
+				{
+					if(piecesLeft[1] > 0)
+					{
+						add(game.getPlayers()[1].getPieces().get(piecesLeft[1] - 1), grid[i][j].getLoc());
+					}
+						
+					piecesLeft[1] --;
 				}
 			}
 		}
@@ -109,6 +146,22 @@ public class Board
 		ArrayList<Node> nodes = move.getNodes();
 		
 		move(nodes.get(0).getLoc(), nodes.get(nodes.size() - 1).getLoc());
+	}
+	
+	/** 
+	 * Draws this board
+	 * 
+	 * @param graphics
+	 */
+	public void draw(Graphics graphics)
+	{
+		for(Node[] row : grid)
+		{
+			for(Node node: row)
+			{
+				node.draw(graphics);
+			}
+		}
 	}
 	
 	/**
