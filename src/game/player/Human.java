6187147008acb57d05cc33c02ed8dedc72867e8d
bencw99 <ben.cohenwang@gmail.com>
2014-09-15 22:11:15 -0700

    CREATED human player class

---
+++ src/game/player/Human.java
@@ -0,0 +1,37 @@
+package game.player;
+
+import game.Move;
+import game.piece.Piece;
+import game.piece.Piece.Loyalty;
+
+import java.util.ArrayList;
+
+/**
+ * A class representing a Human player associated with a checkers game
+ * 
+ * @author Benjamin Cohen-Wang
+ */
+public class Human extends Player
+{
+	/**
+	 * Parameterized constructor, initializes name, pieces, and loyalty
+	 * 
+	 * @param name	the name of this player
+	 * @param loyalty	the loyalty of this player
+	 * @param pieces	the pieces of this player
+	 */
+	public Human(String name, Loyalty loyalty, ArrayList<Piece> pieces)
+	{
+		super(name, loyalty, pieces);
+	}
+
+	/**
+	 * Returns the move executed this turn
+	 * 
+	 * @return	the move to be executed this turn
+	 */
+	public Move getThisTurnMove()
+	{
+		return null;
+	}
+}
