43c7a76d52f1f5d2319f19d533380dc6c9693f8b
bencw99 <ben.cohenwang@gmail.com>
2014-09-28 20:27:07 -0700

    ADDED king functionality

--- src/game/piece/King.java
+++ src/game/piece/King.java
@@ -1,10 +1,13 @@
 package game.piece;
 
 import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.ImageObserver;
 import java.io.IOException;
 import java.util.ArrayList;
 
 import game.Move;
+import game.board.Board;
 import game.board.Location;
 import game.board.Node;
 import game.piece.Piece.Loyalty;
@@ -14,10 +17,11 @@ import game.piece.Piece.Loyalty;
  * 
  * @author Benjamin Cohen-Wang
  */
-public class King extends Piece
+public class King extends Piece implements ImageObserver
 {
 	/**
 	 * Default constructor
+	 * 
 	 * 
 	 * @param loyalty	the value the loyalty of this piece
 	 * @throws IOException 
@@ -53,6 +57,11 @@ public class King extends Piece
 		image = (getLoyalty() == Loyalty.RED) ? RED_KING: BLACK_KING;
 	}
 
+	/**
+	 * Returns the possible nodes this piece can go to
+	 * 
+	 * @return	the array list of possible moves this piece execute
+	 */
 	public ArrayList<Move> getPossibleMoves() 
 	{
 		ArrayList<Move> possibleMoves = new ArrayList<Move>();
@@ -104,22 +113,27 @@ public class King extends Piece
 			return possibleMoves;
 		}
 	}
-
+	
+	/**
+	 * Returns the possible nodes this piece can jump to
+	 * 
+	 * @return	the array list of possible nodes this piece can jump to
+	 */
 	protected ArrayList<ArrayList<Node>> getNextJumps(Location loc)
 	{
 		ArrayList<ArrayList<Node>> retVal = new ArrayList<ArrayList<Node>>();
 		
 		ArrayList<Location> jumps = new ArrayList<Location>();
 		
-		for(int i = -1; i <= 1; i += 1)
+		for(int i = -1 ; i <= 1; i += 2)
 		{
-			for(int j = -1; j <= 1; j += 1)
+			for(int j = -1 ; j <= 1; j += 2)
 			{
 				Location possibleJumpLoc = new Location(loc.getRow() + 2*j, loc.getCol() + 2*i);
 				
 				Location interJumpLoc = new Location(loc.getRow() + j, loc.getCol() + i);
 				
-				if(getNode().getBoard().isValid(possibleJumpLoc) && getNode().getBoard().getPiece(interJumpLoc).getLoyalty() != this.getLoyalty())
+				if(getNode().getBoard().isValid(possibleJumpLoc) && getNode().getBoard().getPiece(possibleJumpLoc) == null && getNode().getBoard().getPiece(interJumpLoc) != null && getNode().getBoard().getPiece(interJumpLoc).getLoyalty() != this.getLoyalty())
 				{
 					jumps.add(possibleJumpLoc);
 				}
@@ -143,8 +157,20 @@ public class King extends Piece
 				
 				for(ArrayList<Node> thisMoveOfCurrent : movesOfCurrent)
 				{
-					thisMoveOfCurrent.add(0, getNode().getBoard().getNode(loc));
-					retVal.add(thisMoveOfCurrent);
+					boolean validMove = true;
+					
+					for(Node jumped : (new Move(thisMoveOfCurrent, getNode().getBoard())).getJumped())
+					{
+						if(jumped.getLoc().equals(new Location((loc.getRow() + jump.getRow())/2, (loc.getCol() + jump.getCol())/2)))
+						{
+							validMove = false;
+						}
+					}
+					if(validMove)
+					{
+						thisMoveOfCurrent.add(0, getNode().getBoard().getNode(loc));
+						retVal.add(thisMoveOfCurrent);
+					}
 				}
 			}
 		}
@@ -154,6 +180,11 @@ public class King extends Piece
 
 	public void draw(Graphics graphics) 
 	{
-		// TODO Auto-generated method stub
+		graphics.drawImage(image, getNode().getLoc().getRow()*Board.NODE_WIDTH + 9, getNode().getLoc().getCol()*Board.NODE_HEIGHT + 9, this);
+	}
+
+	public boolean imageUpdate(Image img, int infoflags, int x, int y, int width, int height)
+	{
+		return false;
 	}
 }
