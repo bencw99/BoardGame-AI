5c477ca326d6c166255ec26f213efb3379561d1c
bencw99 <ben.cohenwang@gmail.com>
2014-10-02 09:27:06 -0700

    CREATED minimax node class and method stubs

---
+++ src/game/player/ai/MinimaxNode.java
@@ -0,0 +1,78 @@
+package game.player.ai;
+
+import java.util.ArrayList;
+
+import game.Move;
+import game.board.Board;
+import game.piece.Piece.Loyalty;
+
+/**
+ * QA class representing a node in the minimax algorithm
+ * 
+ * @author Benjamin Cohen-Wang
+ */
+public class MinimaxNode 
+{
+	/** The minimax depth of this minimax node **/
+	private int minimaxDepth;
+	
+	/** The board of this minimax node **/
+	private Board board;
+	
+	/**
+	 * Parameterized constructor, initializes board and minimax depth to given values
+	 * 
+	 * @param minimaxDepth	the minimaxDepth to be set to
+	 * @param board			the board to be set to
+	 */
+	public MinimaxNode(int minimaxDepth, Board board)
+	{
+		this.minimaxDepth = minimaxDepth;
+		this.board = board;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public Loyalty getLoyalty()
+	{
+		return null;
+	}
+	
+	/**
+	 * Returns the next node of the given move
+	 * 
+	 * @return	the minimax node resulting from the given move
+	 */
+	public MinimaxNode getNextNode(Move move)
+	{
+		return null;
+	}
+	
+	/**
+	 * Returns the possible next moves of this node
+	 * 
+	 * @return	the array list of edges from this node
+	 */
+	public ArrayList<Move> getNextMoves()
+	{
+		return null;
+	}
+	
+	/**
+	 * @return the minimaxDepth
+	 */
+	public int getMinimaxDepth() 
+	{
+		return minimaxDepth;
+	}
+
+	/**
+	 * @return the board
+	 */
+	public Board getBoard() 
+	{
+		return board;
+	}
+}
