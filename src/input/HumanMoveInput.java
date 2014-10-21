3103f42b2bf09a4332fd0220799369ce4495ee65
bencw99 <ben.cohenwang@gmail.com>
2014-10-20 21:59:37 -0700

    CREATED human move input class

---
+++ src/input/HumanMoveInput.java
@@ -0,0 +1,42 @@
+package input;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+/**
+ * A class through which a human player receives input
+ * 
+ * @author Benjamin Cohen-Wang
+ */
+public class HumanMoveInput implements MouseListener
+{
+	public HumanMoveInput()
+	{
+		
+	}
+	
+	public void mouseClicked(MouseEvent e)
+	{
+		
+	}
+
+	public void mouseEntered(MouseEvent e)
+	{
+		
+	}
+
+	public void mouseExited(MouseEvent e)
+	{
+		
+	}
+
+	public void mousePressed(MouseEvent e)
+	{
+		
+	}
+
+	public void mouseReleased(MouseEvent e)
+	{
+		
+	}
+}
