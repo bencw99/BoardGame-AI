0d0346d6b9a86e3fd5e82cf9345bb42b735733be
bencw99 <ben.cohenwang@gmail.com>
2014-11-01 21:01:26 -0700

    MADE small adjustements in ai values

--- src/game/player/ai/AI.java
+++ src/game/player/ai/AI.java
@@ -19,7 +19,7 @@ import java.util.Stack;
 public class AI extends Player
 {
 	/** The depth of the minimax search **/
-	private static final int DEFAULT_MINIMAX_DEPTH = 8;
+	private static final int DEFAULT_MINIMAX_DEPTH = 9;
 	
 	/** The minimax depth of this ai instance **/
 	private final int minimaxDepth;
