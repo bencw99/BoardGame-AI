a5b7e63df7bda97ac3d9b410e3080670b572b622
bencw99 <ben.cohenwang@gmail.com>
2015-01-27 09:46:28 -0800

    Revert "Framework fixes"
    This reverts commit 5e724b918dd03820f04e1c457d9401c5a15205fd.

--- src/game/piece/chessPieces/Pawn.java
+++ src/game/piece/chessPieces/Pawn.java
@@ -8,7 +8,6 @@ import game.move.ChessMove;
 import game.move.Move;
 import game.piece.Piece;
 import game.piece.Piece.Loyalty;
-import game.piece.checkersPieces.King;
 
 /**
  * A class representing a chess pawn
@@ -144,17 +143,5 @@ public class Pawn extends Piece
 		}
 		
 		return possibleMoves;
-	}
-	
-	/**
-	 * Returns a clone of this piece at the given node
-	 * 
-	 * @param node	the node to be cloned to
-	 * @return	the cloned piece
-	 * @throws IOException 
-	 */
-	public Piece clone(Node node) throws IOException
-	{
-		return new Pawn(this, node);
 	}
 }
